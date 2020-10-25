2947f06394360a190b3387f95e31fd24fa2e2a71
Tanesha Goodman <t24good@icloud.com>
2020-10-25 20:47:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  end
-  if int % 3 == 0 && int % 5 == 0
+  else int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
 end
